02a301c0f77eb6b4b05b85c9ca777c01a5a6a05b
petarchou <ppetrov12@abv.bg>
2024-12-12 00:06:27 +0200

    change server to use virtual threads and a bigger queue size (due to minimal cpu time of the requests)

--- src/main/java/org/pesho/servers/JettyServer.java
+++ src/main/java/org/pesho/servers/JettyServer.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.servlet.ServletHolder;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.thread.VirtualThreadPool;
 import org.pesho.servers.servlets.InternalServlet;
 import org.pesho.servers.servlets.ResenderServlet;
 
@@ -11,11 +12,11 @@ public class JettyServer {
     private static final String INTERNAL_PATH = "/loadbalancer";
 
     public void start() {
-        Server server = new Server();
+        Server server = new Server(new VirtualThreadPool());
         try (ServerConnector c = new ServerConnector(server)) {
 
             c.setIdleTimeout(1000);
-            c.setAcceptQueueSize(100);
+            c.setAcceptQueueSize(1000);
             c.setPort(80);
             c.setHost("localhost");
             ServletContextHandler handler = new ServletContextHandler("", true, false);
