a7180ab6a05744cbc69b0365522ed1874a6a702b
petarchou <ppetrov12@abv.bg>
2024-12-13 01:29:38 +0200

    handle edge case of empty server list

--- src/main/java/org/pesho/loadbalancers/RoundRobinBalancer.java
+++ src/main/java/org/pesho/loadbalancers/RoundRobinBalancer.java
@@ -34,6 +34,9 @@ public class RoundRobinBalancer implements LoadBalancer {
 
     public String nextServer() {
         var serverRef = servers; // Keep the same reference for the whole call
+        if (serverRef == null || serverRef.isEmpty()) {
+            throw new IllegalStateException("No active servers available");
+        }
         return serverRef.get(Math.abs(currentServer.getAndIncrement() % serverRef.size()));
     }
 
